de78b3cada183186f7e2f7a86ee696e092451d78
Shakti Gangwar <126997005+Shaktigangwar@users.noreply.github.com>
2023-09-13 12:59:29 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 console.log("Welcome to Spotify");
 // Initialize the variables
 let songIndex = 0;
-let audioElement = new Audio('1.MP3');
+let audioElement = new Audio('1.mp3');
 let masterPlay = document.getElementById('masterPlay')
 let myProgressBar = document.getElementById('myProgressBar');
 let songs=[
-    {songName: "Maan meri jaan", filePath:"C:\Users\DELL\Downloads"},
+    {songName: "Maan meri jaan", filePath: 'C:\Users\DELL\.vscode\project'},
+    {songName: "Maan meri jaan", filePath: 'C:\Users\DELL\.vscode\project'},
+    {songName: "Maan meri jaan", filePath: 'C:\Users\DELL\.vscode\project'},
+    {songName: "Maan meri jaan", filePath: 'C:\Users\DELL\.vscode\project'},
+    {songName: "Maan meri jaan", filePath: 'C:\Users\DELL\.vscode\project'},
     
-    {songName: "Maan meri jaan", filePath:"C:\Users\DELL\Downloads"},
     
-    {songName: "Maan meri jaan", filePath:"C:\Users\DELL\Downloads"},
     
-    {songName: "Maan meri jaan", filePath:"C:\Users\DELL\Downloads"},
-    
-    {songName: "Maan meri jaan", filePath:"C:\Users\DELL\Downloads"},
 ]
 
 //audioElement.play();
 
+// Handle play/pause click;
+masterPlay.addEventListener('click' , ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+    }
+    else{
+        audioElement.pause();
+        masterPlay.classList.remove('fa-circle-pause');
+        masterPlay.classList.add('fa-circle-play');
 
+    }
+})
 
-// Listen to Events
-myProgressBar.addEventListener('timeupdate', ()=>{
+// listen to events
+document.addEventListener('timeupdate', ()=>{
     console.log('timeupdate');
     // update seekbar
 })
+
